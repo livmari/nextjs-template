9de84f2df41f6b783bee98910e2821220ebe7851
Liv Mari <liv.alterskjaer@icloud.com>
2022-10-16 14:20:11 +0200

    #8 ts installed and used (oopsies)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
   return (
     <div>
       <h1 className={'text-3xl font-bold'}>Hello world</h1>
     </div>
   )
 }
+
+export default Home
